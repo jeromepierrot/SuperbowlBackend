4d250a40a38b198b456d156c4d217bf3a0ed1c66
jpierrot <je.pierrot@gmail.com>
2023-06-22 10:11:00 +0200

    setup: OpenAPI and Actuator NB : temp disable of Spring security for dev puposes

--- src/main/java/fr/jpierrot/SuperbowlBackend/SuperbowlBackendApplication.java
+++ src/main/java/fr/jpierrot/SuperbowlBackend/SuperbowlBackendApplication.java
@@ -1,8 +1,11 @@
 package fr.jpierrot.SuperbowlBackend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")}) /* this annotation sets up OpenApi for https*/
 @SpringBootApplication
 public class SuperbowlBackendApplication {
 
